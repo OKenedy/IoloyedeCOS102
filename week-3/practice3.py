2e48ac364ea54d8d672f630789fbe4ac4e6941a1
OKenedy <ayomideademide72@gmail.com>
2025-04-16 11:01:49 +0100

    (second commit)

---
+++ week-3/practice3.py
@@ -0,0 +1,14 @@
+# COUPE DE ESCRIVA 2023: FOOTBALL PICKS
+
+print("Welcome to the COUPE DE ESCRIVA 2023: FOOTBALL PICKS \n")
+
+captain = {'Madiba': 'Chubby Obiora-Okafo', 'Blue-Jays': 'Christopher Uweh', 'Cirok': 'Alexander', 'TSG Walkers': 'Ikechukwu'}
+goalkeepers = {'Madiba': 'Chubby Obiora-Okafo', 'Blue-Jays': 'Oladimeji Abaniwondea/Jeffery Awagu', 'Cirok': 'Timileyin Pearse/Izuako Jeremy', 'TSG Walkers': 'Ayomide Ojituku'}
+
+print("Captains:")
+for pick in captain:
+    print(f"{pick}: {captain[pick]}")
+
+print("\nGoalkeepers:")
+for pick in goalkeepers:
+    print(f"{pick}: {goalkeepers[pick]}")
